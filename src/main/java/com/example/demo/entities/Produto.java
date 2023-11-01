3aa81f5bfe69491b4a9b12abe6834366c9a13510
Marcella-acrg <marcellaacoelho@gmail.com>
2023-10-31 21:12:58 -0300

    primeiro commit

---
+++ src/main/java/com/example/demo/entities/Produto.java
@@ -0,0 +1,80 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Produtos")
+public class Produto implements Serializable{    
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    private Integer preco;
+
+    public Produto() {    	
+    }
+
+	public Produto(Integer id, String nome, Integer preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Integer preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+    
+    
+}
+    
